48e342794a90dcf6322a95a846d67b34d20b3ca3
Margarida <margaridammcm@gmail.com>
2022-12-20 22:38:37 -0300

    add botao excluir

--- src/paginas/Pedidos/index.tsx
+++ src/paginas/Pedidos/index.tsx
@@ -20,6 +20,18 @@ const Pedidos = () => {
         .catch(erro => console.log(erro))
     }, [])
 
+    const excluir = (pedido: IPedido) => {
+        const token = sessionStorage.getItem('token')
+        axios.delete('http://localhost:8000/pedidos/' + pedido.id, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(() => {
+            setPedidos(pedidos.filter(p => p.id !== pedido.id))
+        })
+        .catch(erro => console.log(erro))
+    }
+
     return(
         <section className="pedidos">
             <h1>Meus Pedidos</h1>
@@ -31,6 +43,7 @@ const Pedidos = () => {
                     <li>Entrega realizada em: <strong>{new Date(pedido.entrega).toLocaleDateString()}</strong></li>
                 </ul>
                 <AbBotao texto="Detalhes"></AbBotao>
+                <AbBotao onClick={() => excluir(pedido)} texto="Excluir"></AbBotao>
             </div>))}
         </section>
     )
